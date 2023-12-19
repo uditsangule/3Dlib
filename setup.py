c84bd3c262a8db36857ed8aeb45f884e19fa0bd6
uditsangule <uditsangule@gmail.com>
2023-12-19 13:50:14 +0530

    install_req added in setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from setuptools import setup , find_packages
-
+from pathlib import Path
+from pkg_resources import parse_requirements
 VERSION = '0.2'
 DESC = '3D lib for SceneRecon'
 LONG_DESC = 'First verion of 3dlib'
+with Path('requirements.txt').open() as f: install_req =  [str(r).split('~')[0] for r in parse_requirements(f) if not str(r).__contains__('-')]
 
 setup(name='lib3d' , version=VERSION,author='udit.S',
       author_email='uditsangule@gmail.com',description=DESC,
       long_description=LONG_DESC,
       packages=find_packages(),
-      requires=[],
+      requires=install_req,
       keywords=['python3','find package'],
       classifiers=['Operating System :: Linux'],python_requires='>=3.8')
