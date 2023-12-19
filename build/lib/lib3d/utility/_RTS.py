9c62e17cfb13a5324b277887c95d4b01b3e3b751
uditsangule <uditsangule@gmail.com>
2023-12-19 18:01:13 +0530

    linebreaks optimized

--- build/lib/lib3d/utility/_RTS.py
+++ build/lib/lib3d/utility/_RTS.py
@@ -13,10 +13,12 @@ def get_rotmat(vec1, vec2):
     a = vec1/np.linalg.norm(vec1) if np.linalg.norm(vec1)!=1 else vec1
     b = vec2/np.linalg.norm(vec2) if np.linalg.norm(vec2)!=1  else vec2
     v = np.cross(a, b)
-    if any(v): #if not all zeros then
-        c = np.dot(a, b)
-        s = np.linalg.norm(v)
-        kmat = np.array([[0, -v[2], v[1]], [v[2], 0, -v[0]], [-v[1], v[0], 0]])
-        return np.eye(3) + kmat + kmat.dot(kmat) * ((1 - c) / (s ** 2))
-    else:
-        return np.eye(3)
+    if not any(v): return np.eye(3)
+    #if not all zeros then
+    c = np.dot(a, b)
+    s = np.linalg.norm(v)
+    kmat = np.array([[0, -v[2], v[1]],
+                     [v[2], 0, -v[0]],
+                     [-v[1], v[0], 0]])
+    return np.eye(3) + kmat + kmat.dot(kmat) * ((1 - c) / (s ** 2))
+
