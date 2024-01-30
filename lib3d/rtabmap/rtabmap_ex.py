9cbddda345da78fedc56353f6252fa2242c0f109
uditsangule <uditsangule@gmail.com>
2024-01-30 20:50:09 +0530

    fix : intendations and linespace feature : vector angle calculation func added

--- lib3d/rtabmap/rtabmap_ex.py
+++ lib3d/rtabmap/rtabmap_ex.py
@@ -1,9 +1,9 @@
 import os
 path = os.path.dirname(__file__)
-def export(inputdb , filex='_',ascii=True , ba=True , optim=True , poses=True , formatpose=11):
+def export(inputdb , filex='_',ascii=True , ba=False , optim=True , poses=True,mesh=False , formatpose=11):
     stat = 0
     return os.system(f"{path}/rtabmap-export {'--custom_optimization' if optim else ''} {'--ba' if ba else ''} \
-                      --images_id --ground_normals_up --prop_radius_factor 0.05 --poses --poses_format {formatpose} --outputdir {os.path.dirname(inputdb) + os.sep} --output {filex} {'--ascii' if ascii else ''} {inputdb}")
+                      --images_id --ground_normals_up {'--mesh' if mesh else ''} --prop_radius_factor 0.05 --poses --poses_format {formatpose} --outputdir {os.path.dirname(inputdb) + os.sep} --output {filex} {'--ascii' if ascii else ''} {inputdb}")
 
 def detectloopclosure(inputdb , iter=12, rmax=3 , rmin=0 ):
     return os.system(f"{path}/rtabmap-detectMoreLoopClosures -rx {rmin} -r {rmax} -i {iter} --intra {inputdb}")
